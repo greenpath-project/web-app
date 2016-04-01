6d6ef6b36706aac2822e225cc45a3c99b0f468b1
Jessica Lacaze <lacaze.jessica@gmail.com>
2016-04-01 17:58:52 +0200

    Moteur de recherche opérationnel

--- public/app/components/engine/engine.ctrl.js
+++ public/app/components/engine/engine.ctrl.js
@@ -1,6 +1,32 @@
-angular.module('greenPathApp').controller('EngineCtrl', ['$scope', 'Engine', function($scope, Engine){
+angular.module('greenPathApp').controller('EngineCtrl', ['$scope', 'Engine','$http', function($scope, Engine,$http){
 
-    $scope.test = 'lool';
+    $scope.resultSearch = [];
+    $scope.longitude ="";
+    $scope.latitude ="";
+    $scope.ville="";
+    $scope.departement="";
 
+    $scope.onChangeInput = function () {
+        $scope.resultSearch.length = 0;
 
+        if($scope.longitude.length >= 3 || $scope.latitude.length >= 3 || $scope.ville.length >= 4 || $scope.departement.length >= 3){
+        	
+        	var config = {
+	            params: {
+	                lng:$scope.longitude,
+	                lat:$scope.latitude,
+	                ville:$scope.ville,
+	                departement:$scope.departement
+	            }
+	        }
+	    
+	        $http.get('/api/captures/releve',config)
+	            .success(function(data){
+	            	console.log(data);
+	                data.forEach(function(capture){
+	                    $scope.resultSearch.push(capture);
+	                });              
+	            });
+        }
+    }
 }]);
