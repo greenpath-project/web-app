f3fbca2afcdc25b11d4fb2361e589dd244257e03
Julien <julien.maupetit@epsi.fr>
2016-05-26 12:42:52 +0200

    Suppression répertoire MongoDB suite au changement de BDD, modification du script d'installation de la BDD PostgreSQL.

--- doc/PostgreSQL/install.sql
+++ doc/PostgreSQL/install.sql
@@ -20,26 +20,9 @@ CREATE DATABASE greenpath
 
 ALTER DATABASE greenpath
   SET search_path = "$user", public, tiger;
-
-
 	   
 -- Enable PostGIS (includes raster)
 CREATE EXTENSION postgis;
--- Enable Topology
-CREATE EXTENSION postgis_topology;
-CREATE EXTENSION postgis_topology;
--- Enable PostGIS Advanced 3D 
--- and other geoprocessing algorithms
--- sfcgal not available with all distributions
-CREATE EXTENSION postgis_sfcgal;
--- fuzzy matching needed for Tiger
-CREATE EXTENSION fuzzystrmatch;
--- rule based standardizer
-CREATE EXTENSION address_standardizer;
--- example rule data set
-CREATE EXTENSION address_standardizer_data_us;
--- Enable US Tiger Geocoder
-CREATE EXTENSION postgis_tiger_geocoder;
 
 -- Table: public.departements
 
