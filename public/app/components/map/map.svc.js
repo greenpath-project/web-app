10ac9a5307ba44f4003215b27679b5d9561341b8
Julien <julien.maupetit@epsi.fr>
2016-05-30 10:57:26 +0200

    Changement du lien du serveur en prod.

--- public/app/components/map/map.svc.js
+++ public/app/components/map/map.svc.js
@@ -4,13 +4,13 @@ angular.module('greenPathApp').factory('Map', [function(){
         return new ol.source.Vector({
             format: new ol.format.GeoJSON(),
             url: function(extent) {
-                /*return 'http://greenpath.jumpt.fr/geoserver/greenpath/ows?service=WFS&' +
-                    'version=1.1.0&request=GetFeature&typeName=greenpath:captures&' +
-                    'outputFormat=application%2Fjson&bbox=' + extent.join(',') + ',EPSG:3857&srsname=EPSG:3857';*/
-                    
-                return 'http://localhost:3000/geoserver/greenpath/ows?service=WFS&' +
+                return 'http://greenpath.jumpt.fr/geoserver/greenpath/ows?service=WFS&' +
                     'version=1.1.0&request=GetFeature&typeName=greenpath:captures&' +
                     'outputFormat=application%2Fjson&bbox=' + extent.join(',') + ',EPSG:3857&srsname=EPSG:3857';
+                    
+                /*return 'http://localhost:3000/geoserver/greenpath/ows?service=WFS&' +
+                    'version=1.1.0&request=GetFeature&typeName=greenpath:captures&' +
+                    'outputFormat=application%2Fjson&bbox=' + extent.join(',') + ',EPSG:3857&srsname=EPSG:3857';*/
             },
             strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
                 maxZoom: 1
